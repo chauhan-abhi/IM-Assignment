4a45056171acefa2fab8d4af7ab2b286b2f14d78
chauhan-abhi <chauhanabhi1245@gmail.com>
2019-07-10 12:58:42 +0530

    navigation completed using navigation components

--- app/src/main/java/com/example/demo_day1/MainActivity.kt
+++ app/src/main/java/com/example/demo_day1/MainActivity.kt
@@ -2,11 +2,77 @@ package com.example.demo_day1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navController: NavController
+    lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpNavigation()
     }
+
+    private fun setUpNavigation() {
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigationView)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navigationView, navController)
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(
+                this,
+                R.id.nav_host_fragment
+            ),
+            drawerLayout
+        )
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        menuItem.isChecked = true
+        drawerLayout.closeDrawers()
+
+        when (menuItem.itemId) {
+            R.id.first -> navController.navigate(R.id.firstFragment)
+            R.id.second -> navController.navigate(R.id.secondFragment)
+            R.id.third -> navController.navigate(R.id.thirdFragment)
+            R.id.fourth -> navController.navigate(R.id.fourthFragment)
+            else -> navController.navigate(R.id.homeFragment)
+
+        }
+        return true
+    }
+
 }
--- app/src/main/java/com/example/demo_day1/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/demo_day1/fragments/HomeFragment.kt
@@ -6,25 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import com.example.demo_day1.R
-import androidx.appcompat.app.AppCompatActivity
 
 
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        (activity as AppCompatActivity).supportActionBar!!.hide()
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        (activity as AppCompatActivity).supportActionBar!!.show()
-
-    }
 
 }
