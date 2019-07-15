a6bf876e2a3a293c479a9d72c60e935f07b1a16d
chauhan-abhi <chauhanabhi1245@gmail.com>
2019-07-15 13:14:53 +0530

    async task for fetching user detail using mocky.io completed

--- app/src/main/java/com/example/demo_day1/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/demo_day1/fragments/SecondFragment.kt
@@ -1,21 +1,38 @@
 package com.example.demo_day1.fragments
 
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.demo_day1.R
 import com.example.demo_day1.adapters.ContactListAdapter
 import com.example.demo_day1.model.Contact
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import com.example.demo_day1.utils.REMOTE_URL
+import com.example.demo_day1.utils.isNetworkStatusAvailable
+import com.example.demo_day1.utils.showSnackBar
+import com.google.android.material.snackbar.Snackbar
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.Buffer
+
 
 class SecondFragment : Fragment() {
 
 
     private lateinit var rvContact: RecyclerView
+    private lateinit var progressBar: ProgressBar
     val contactList: ArrayList<Contact> = ArrayList()
 
     override fun onCreateView(
@@ -25,12 +42,25 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_second, container, false)
         rvContact = view.findViewById(R.id.rv_contact_list)
+        progressBar = view.findViewById(R.id.progressBar)
 
-        addContacts()
+        //addContacts()
         rvContact.layoutManager = LinearLayoutManager(context)
+        rvContact.setHasFixedSize(true)
         rvContact.adapter = ContactListAdapter(contactList)
 
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isNetworkStatusAvailable(context!!)) {
+            FetchUserAsyncTask().execute()
+        } else {
+            progressBar.visibility = View.GONE
+            //Toast.makeText(activity, "No internet connection", Toast.LENGTH_LONG).show()
+            showSnackBar(activity as FragmentActivity, "No Internet Connection")
+        }
     }
 
     private fun addContacts() {
@@ -46,6 +76,110 @@ class SecondFragment : Fragment() {
                 )
             )
         }
+    }
+
+    inner class FetchUserAsyncTask : AsyncTask<Void, Void, ArrayList<Contact>>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            progressBar.visibility = View.VISIBLE
+            rvContact.visibility = View.GONE
+        }
+
+        override fun onPostExecute(result: ArrayList<Contact>) {
+            super.onPostExecute(result)
+            progressBar.visibility = View.GONE
+            rvContact.visibility = View.VISIBLE
+            rvContact.adapter = ContactListAdapter(result)
+        }
+
+        override fun doInBackground(vararg url: Void): ArrayList<Contact> {
+            var urlConnection: HttpURLConnection? = null
+            var reader: BufferedReader? = null
+
+            var userResponse: String
+
+            try {
+                urlConnection =
+                    URL("http://www.mocky.io/v2/5d2c25c53100000e00f5a68e").openConnection() as HttpURLConnection
+
+                urlConnection.requestMethod = "GET"
+                urlConnection.setRequestProperty("Content-Type", "application/json")
+                urlConnection.setRequestProperty("Accept", "application/json")
+                urlConnection.connect()
+
+                val inString = streamToString(urlConnection.inputStream)
+                return parseUserResponse(inString)
+
+                /*urlConnection.inputStream.let {
+                    if (it == null) {
+                        return ArrayList()
+                    }
+                    val buffer = StringBuffer()
+                    reader = BufferedReader(InputStreamReader(it))
+
+                    var line: String
+
+                    if (reader != null) {
+                        while (reader.readLine() != null) {
+                            line = reader!!.readLine()
+                            buffer.append(line + "\n")
+                        }
+                    }
+
+                    if (buffer.isEmpty()) {
+                        return ArrayList()
+                    }
+                    userResponse = buffer.toString()
+                    return parseUserResponse(userResponse)
+                }*/
+
+            } catch (e: IOException) {
+                Log.e("IOERROR", e.message + "")
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect()
+                }
+            }
+            return contactList
+        }
+    }
+
+    fun streamToString(inputStream: InputStream): String {
+
+        val bufferReader = BufferedReader(InputStreamReader(inputStream))
+        var line: String
+        var result = ""
+
+        try {
+            do {
+                line = bufferReader.readLine()
+                if (line != null) {
+                    result += line
+                }
+            } while (line != null)
+            inputStream.close()
+        } catch (ex: Exception) {
+
+        }
+
+        return result
+    }
+
+    private fun parseUserResponse(userResponse: String): ArrayList<Contact> {
+        val jsonArray = JSONArray(JSONObject(userResponse).getString("results"))
+        for (i in 0 until jsonArray.length()) {
+             val user = jsonArray[i] as JSONObject
+             contactList.add(
+                 Contact(
+                     user.getString("personName"),
+                     user.getString("personEmail"),
+                     user.getLong("contactNumber").toString(),
+                     "https://avatars1.githubusercontent.com/u/20797673?s=460&v=4"
+                 )
+             )
+         }
+        return contactList
     }
 
 
--- app/src/main/java/com/example/demo_day1/utils/AndroidExtensions.kt
+++ app/src/main/java/com/example/demo_day1/utils/AndroidExtensions.kt
@@ -1,11 +1,45 @@
 package com.example.demo_day1.utils
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.example.demo_day1.R
 import org.json.JSONObject
 import java.io.InputStream
+import android.R.attr.duration
+import com.google.android.material.snackbar.Snackbar
+import android.app.Activity
+import android.view.View
+
+
+fun isNetworkStatusAvailable(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+    connectivityManager?.let { it ->
+        if (android.os.Build.VERSION.SDK_INT > 23) {
+            it.getNetworkCapabilities(it.activeNetwork)?.let {
+                if (it.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)) {
+                    return true
+                }
+
+            }
+        } else {
+            it.activeNetworkInfo?.let {
+                return it.isConnectedOrConnecting
+            }
+
+        }
+    }
+    return false
+}
+
+fun showSnackBar(activity: Activity, message: String) {
+    val rootView = activity.window.decorView.findViewById<View>(android.R.id.content)
+    Snackbar.make(rootView, message, Snackbar.LENGTH_LONG).show()
+}
+
 
 fun ImageView.loadImg(imageUrl: String?) =
     Glide.with(context)
@@ -14,7 +48,7 @@ fun ImageView.loadImg(imageUrl: String?) =
         .into(this)
 
 fun readJSONFromAsset(applicationContext: Context): JSONObject {
-    val jsonfile: String = applicationContext.assets.open("ela.json").bufferedReader().use {it.readText()}
+    val jsonfile: String = applicationContext.assets.open("ela.json").bufferedReader().use { it.readText() }
 
     return JSONObject(jsonfile)
 }
